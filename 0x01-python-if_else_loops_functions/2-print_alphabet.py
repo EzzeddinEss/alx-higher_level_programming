a6f3e28538eebfeaa3bd3e80e46d7dc638cebb77
Ezzeddin Essam <ej_dozze95@outlook.com>
2023-10-03 08:14:22 -0700

    00.99

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-# 2-print_alphabet.py
 
 for i in range(ord('a'), ord('z') + 1):
     print("{}".format(chr(i)), end="")
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-# 3-print_alphabt.py
 
 for i in range(ord('a'), ord('z') + 1):
     if chr(i) != 'q' and chr(i) != 'e':
