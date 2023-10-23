b0a16ea97c6f307a785dbf761946e0838310358d
Ezzeddin Essam <ej_dozze95@outlook.com>
2023-10-23 06:29:03 -0700

    Safe list printing

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    idx = 0
+    try:
+        for idx in range(x):
+            print(my_list[idx], end = '')
+            idx += 1
+    except IndexError:
+        None
+    print()
+    return idx
