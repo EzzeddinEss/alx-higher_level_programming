484370beb993f6a0aa7566eed8a2f24d62da6441
Ezzeddin Essam <ej_dozze95@outlook.com>
2023-11-07 06:48:16 -0800

    To JSON string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""JSON representation of an object function definition"""
+
+
+def to_json_string(my_obj):
+    """JSON representation of an object"""
+    return json.dumps(my_obj)
