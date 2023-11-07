0334e18ac7b56220a79f533098d91f56a3d4ac8d
Ezzeddin Essam <ej_dozze95@outlook.com>
2023-11-07 06:49:48 -0800

    code fix

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -2,6 +2,9 @@
 """JSON representation of an object function definition"""
 
 
+import json
+
+
 def to_json_string(my_obj):
     """JSON representation of an object"""
     return json.dumps(my_obj)
