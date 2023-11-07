7bb55ef543094773d08301f6149980aff5cca313
Ezzeddin Essam <ej_dozze95@outlook.com>
2023-11-07 06:55:36 -0800

    From JSON string to Object

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""a function that returns an object definition"""
+
+
+import json
+
+
+def from_json_string(my_str):
+    """object represented by a JSON string function"""
+    return json.load(my_str)
