a726d9e0bde7ce3897c0167936c34fbc43e5a350
Ezzeddin Essam <ej_dozze95@outlook.com>
2023-11-13 08:25:44 -0800

    And now, the Square!

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""A model for a subclass that inherits from Rectangle"""
+from models.rectangle import Rectangle
+
+class Square(Rectangle):
+    """a subclass from rectangle class"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        '''class constructor'''
+        super().__init__(id, x, y, size, size)
+
+    def __str__(self):
+        """Return a string representation of the square"""
+        return '[{}] ({}) {}/{} - {}/{}'.format(
+                type(self).__name__,
+                self.id,
+                self.x,
+                self.y,
+                self.width,
+                self.height)
