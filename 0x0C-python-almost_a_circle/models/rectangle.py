c688b54f1e01eaf825e66016fd3b73796b3a94a2
Ezzeddin Essam <ej_dozze95@outlook.com>
2023-11-13 06:23:46 -0800

    Display #0

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -14,47 +14,58 @@ class Rectangle(Base):
         self.x = x
         self.y = y
 
-        @property
-        def width(self):
-            return self.__width
+    @property
+    def width(self):
+        return self.__width
 
-        @width.setter
-        def width(self, value):
-            self.validate_integer("width", value, False)
-            self.__width = value
+    @width.setter
+    def width(self, value):
+        self.validate_integer("width", value, False)
+        self.__width = value
 
-        @property
-        def height(self):
-            return self.__height
+    @property
+    def height(self):
+        return self.__height
 
-        @height.setter
-        def height(self, value):
-            self.validate_integer("height", value, False)
-            self.__height = value
+    @height.setter
+    def height(self, value):
+        self.validate_integer("height", value, False)
+        self.__height = value
 
-        @property
-        def x(self):
-            return self.x
+    @property
+    def x(self):
+        return self.__x
 
-        @x.setter
-        def x(self, value):
-            self.validate_integer("x", value)
-            self.__x = value
+    @x.setter
+    def x(self, value):
+        self.validate_integer("x", value)
+        self.__x = value
 
-        @property
-        def y(self):
-            return self.y
+    @property
+    def y(self):
+        return self.__y
 
-        @y.setter
-        def y(self, value):
-            self.validate_integer("y", value)
-            self.__y = value
+    @y.setter
+    def y(self, value):
+        self.validate_integer("y", value)
+        self.__y = value
 
-        def validate_integer(self, name, value, equal=True):
-            """a method to validate all setter"""
-            if type(value) is not int:
-                raise TypeError("{} must be an integer".format(name))
-            if not equal and value <= 0:
-                raise ValueError("{} must be > 0".format(name))
-            if equal and value < 0:
-                raise ValueError("{} must be >= 0".format(name))
+    def validate_integer(self, name, value, equal=True):
+        """a method to validate all setter"""
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if not equal and value <= 0:
+            raise ValueError("{} must be > 0".format(name))
+        if equal and value < 0:
+            raise ValueError("{} must be >= 0".format(name))
+
+    def area(self):
+        """Calculate the area of the rectangle"""
+        return self.width * self.height
+
+    def display(self):
+        """Display the rectangle with '#' characters"""
+        string = '\n' * self.y
+        for i in range(self.height):
+            string += ' ' * self.x + '#' * self.width + '\n'
+        print(string, end=" ")
