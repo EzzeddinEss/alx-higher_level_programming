30a94fc4ab122fd45a53d66e7998857ca9c84e99
Ezzeddin Essam <ej_dozze95@outlook.com>
2023-11-13 03:44:47 -0800

    Validate attributes

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -20,6 +20,7 @@ class Rectangle(Base):
 
         @width.setter
         def width(self, value):
+            self.validate_integer("width", value, False)
             self.__width = value
 
         @property
@@ -28,6 +29,7 @@ class Rectangle(Base):
 
         @height.setter
         def height(self, value):
+            self.validate_integer("height", value, False)
             self.__height = value
 
         @property
@@ -36,6 +38,7 @@ class Rectangle(Base):
 
         @x.setter
         def x(self, value):
+            self.validate_integer("x", value)
             self.__x = value
 
         @property
@@ -44,4 +47,14 @@ class Rectangle(Base):
 
         @y.setter
         def y(self, value):
+            self.validate_integer("y", value)
             self.__y = value
+
+        def validate_integer(self, name, value, equal=True):
+            """a method to validate all setter"""
+            if type(value) is not int:
+                raise TypeError("{} must be an integer".format(name))
+            if not equal and value <= 0:
+                raise ValueError("{} must be > 0".format(name))
+            if equal and value < 0:
+                raise ValueError("{} must be >= 0".format(name))
