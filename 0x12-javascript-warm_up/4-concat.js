920c89710d3b9bed7b7e888b97c03c3c8326dd1d
Ezzeddin <ej_dozze95@outlook.com>
2024-02-13 08:28:08 -0800

    Create a sentence

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const argsCount = process.argv.slice(2);
+
+if (argsCount.length >= 2) {
+  console.log(argsCount[0] + " is " + argsCount[1]);
+} else if (argsCount.length === 1) {
+  console.log(argsCount[0] + " is " + "undefined");
+} else {
+  console.log("undefined" + " is " + "undefined");
+}
