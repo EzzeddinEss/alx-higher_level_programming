dec0200a037ffa43a36261b50dccb58574446e48
Ezzeddin <ej_dozze95@outlook.com>
2024-02-13 08:30:16 -0800

    Create a sentence

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -2,9 +2,9 @@
 const argsCount = process.argv.slice(2);
 
 if (argsCount.length >= 2) {
-  console.log(argsCount[0] + " is " + argsCount[1]);
+  console.log(argsCount[0] + ' is ' + argsCount[1]);
 } else if (argsCount.length === 1) {
-  console.log(argsCount[0] + " is " + "undefined");
+  console.log(argsCount[0] + ' is ' + 'undefined');
 } else {
-  console.log("undefined" + " is " + "undefined");
+  console.log('undefined' + ' is ' + 'undefined');
 }
